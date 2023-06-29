d250268427d97b8f1c7386e5a96380a3cfd16a03
chris-eminence <emichris380@gmail.com>
2023-06-29 02:38:01 +0100

    changes

--- lib/SplashScreen.dart
+++ lib/SplashScreen.dart
@@ -13,8 +13,6 @@ class _SplashScreenState extends State<SplashScreen> {
     return const Scaffold(
 
       backgroundColor: Colors.black,
-      body: Center(child: Text("This is the start of your application, and this is suppose to be the splash screen")),
-
       body: Center
         (child: Text(
           "Welcome to Netisens Gallery",
